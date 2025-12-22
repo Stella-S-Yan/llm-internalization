cbfbfd7f11b8f0dfa5ddebac649ee11a04df1386
Stella Yan <stellasyan@google.com>
2025-12-22 18:15:28 +0000

    remove faiss

--- combined_data/p7_2_emb_alignment_prefix.py
+++ combined_data/p7_2_emb_alignment_prefix.py
@@ -26,7 +26,6 @@ import os
 import itertools
 from transformers import get_cosine_schedule_with_warmup, get_inverse_sqrt_schedule, get_polynomial_decay_schedule_with_warmup
 import numpy as np
-import faiss
 from tqdm import tqdm
 import re
 
