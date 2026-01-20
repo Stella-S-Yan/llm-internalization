69c7c73761c8c764bafef00897f53b81ba0d97c5
Stella Yan <stellasyan@google.com>
2026-01-20 21:08:15 +0000

    continue training does not work, remove

--- combined_data/f1_2_eval_sft_ddp_checkpoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set +e
-
-STEP=$1   # first argument passed to the script
-
-python merge_save_sft_model.py --checkpoint_step $STEP
-
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-torchrun --nproc_per_node=8 eval_sft_ddp.py
