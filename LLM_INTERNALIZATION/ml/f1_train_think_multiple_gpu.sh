cff269a359579979380364dd6539037b4d44bfc2
Su Yan <stellasyan@google.com>
2026-05-08 18:22:17 +0000

    update ml-20m launch script

--- LLM_INTERNALIZATION/ml/f1_train_think_multiple_gpu.sh
+++ LLM_INTERNALIZATION/ml/f1_train_think_multiple_gpu.sh
@@ -3,17 +3,17 @@ set +e
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
-# Best for 20m
-# params="--LR 2e-4 --WEIGHT_DECAY 0.005 --WARMUP_STEPS 2000 \
-#         --TRAIN_BATCH_SIZE 16 --ACC_STEP 1 \
-#         --LORA_RANK 32 --LORA_RATIO 2 --LORA_DROPOUT 0.05 \
-#         --TOTAL_STEPS 200000 --RUN_NUM 0 --CHECK_POINT 0"
-
-# For 1m
+# For 20m
 params="--LR 2e-4 --WEIGHT_DECAY 0.005 --WARMUP_STEPS 2000 \
-        --TRAIN_BATCH_SIZE 4 --ACC_STEP 1 \
+        --TRAIN_BATCH_SIZE 16 --ACC_STEP 1 \
         --LORA_RANK 32 --LORA_RATIO 2 --LORA_DROPOUT 0.05 \
         --TOTAL_STEPS 200000 --RUN_NUM 0 --CHECK_POINT 0"
+
+# For 1m
+# params="--LR 2e-4 --WEIGHT_DECAY 0.005 --WARMUP_STEPS 2000 \
+#         --TRAIN_BATCH_SIZE 4 --ACC_STEP 1 \
+#         --LORA_RANK 32 --LORA_RATIO 2 --LORA_DROPOUT 0.05 \
+#         --TOTAL_STEPS 200000 --RUN_NUM 0 --CHECK_POINT 0"
 
 
 torchrun \
