774b5f02ed20f1c538f4a39c06f591b3893a44e5
Su Yan <stellasyan@google.com>
2026-05-08 18:16:28 +0000

    disable Xet for older version of hf

--- push_data_to_hf/push.py
+++ push_data_to_hf/push.py
@@ -1,4 +1,6 @@
 import os
+os.environ["HF_HUB_DISABLE_XET"] = "1"
+
 from huggingface_hub import HfApi
 from huggingface_hub import login
 import fnmatch
@@ -12,40 +14,40 @@ api = HfApi()
 
 base = "/usr/local/google/home/stellasyan/Documents/workspace"
 
-# # Upload model
-# model_base = base + "/model"
-# for root, dirs, files in os.walk(model_base):
-#     for f in files:
-#         if f.endswith(".tgz") and not fnmatch.fnmatch(f, "*merged_think_sft_model_*.tgz"):
-#             local_path = os.path.join(root, f)
-#             rel_path = os.path.relpath(local_path, base)
+# Upload model
+model_base = base + "/model"
+for root, dirs, files in os.walk(model_base):
+    for f in files:
+        if f.endswith(".tgz") and not fnmatch.fnmatch(f, "*merged_think_sft_model_*.tgz"):
+            local_path = os.path.join(root, f)
+            rel_path = os.path.relpath(local_path, base)
 
-#             print(f)
-#             print("here")
-#             api.upload_file(
-#                 path_or_fileobj=local_path,
-#                 path_in_repo=f"model/{f}",
-#                 repo_id="UsernameAlreadyExitsts/llm_internalization",
-#                 repo_type="dataset",
-#                 commit_message=f"Update {rel_path}",
-#             )
+            print(f)
+            print("here")
+            api.upload_file(
+                path_or_fileobj=local_path,
+                path_in_repo=f"model/{f}",
+                repo_id="UsernameAlreadyExitsts/llm_internalization",
+                repo_type="dataset",
+                commit_message=f"Update {rel_path}",
+            )
 
 
 # Upload processed_data
 data_base = base + "/processed_data"
 for root, dirs, files in os.walk(data_base):
     for f in files:
-        if f.startswith("Lepard_10k"):  # "Amazon", "Lepard_10k"
-            local_path = os.path.join(root, f)
-            rel_path = os.path.relpath(local_path, base)
+        local_path = os.path.join(root, f)
+        rel_path = os.path.relpath(local_path, base)
 
-            api.upload_file(
-                path_or_fileobj=local_path,
-                path_in_repo=f"processed_data/{f}",
-                repo_id="UsernameAlreadyExitsts/llm_internalization",
-                repo_type="dataset",
-                commit_message=f"Update {rel_path}",
-            )
+        api.upload_file(
+            path_or_fileobj=local_path,
+            path_in_repo=f"processed_data/{f}",
+            repo_id="UsernameAlreadyExitsts/llm_internalization",
+            repo_type="dataset",
+            commit_message=f"Update {rel_path}",
+        )
+
 
 # Upload a final model
 model_base = base + "/model"
